801f6eb5b98c92d6518ab35cf214e2ca196cfe51
aghash <44614133+aghash@users.noreply.github.com>
2021-10-05 14:54:13 +0530

    Add files via upload

--- Code.py
+++ Code.py
@@ -4,27 +4,31 @@ import ast
 from pathlib import Path 
 pyton_directory=os.getcwd()
 content ={}
-file_loc=Path((pyton_directory)+"//data.txt")
+file_loc=Path((pyton_directory)+"//api_data.txt")
 flg=0
 if file_loc.exists():
-    file=open("data.txt",'r')
+    file=open("api_data.txt",'r')
     content=ast.literal_eval(file.read())
     file.close()
-    file=open("data.txt",'w+')
+    file=open("api_data.txt",'w+')
     flg=1
 else:    
-    file=open("data.txt",'w+')
-try:    
-    address=input("Enter api address : ")
-    result=requests.get(address)
-except:
-    address=input("Enter valid api address : ")
-finally:
-    file.write(str(result.json()))
-    for item,index in result.json().items():
-        if item!= 'categories':
-            if flg==0:
-                print(str(item)+' has changed from None to '+str(index))
-            else:
-                print(str(item)+' has changed from '+ str(content.get(item)) +' to '+str(index))
+    file=open("api_data.txt",'w+')
+while True:
+    try:    
+        address=input("Enter api address : ")
+        result=requests.get(address)
+    except:
+            print("Enter valid api address")
+    else:
+        break
+file.write(str(result.json()))
+if flg==0:
+    print("This is the first execution")
+for item,index in result.json().items():
+    if item!= 'categories':
+      if flg==0:
+          print(str(item)+' is '+str(index))
+      else:
+           print(str(item)+' has changed from '+ str(content.get(item)) +' to '+str(index))
 file.close()
